c65a1604a851e8f96054ec10c17e345dd0a5280d
ricardo <ricardoramos.usp@gmail.com>
2015-07-06 15:29:10 -0300

    Acrescentando o Before

--- src/main/java/br/com/caelum/aula/MaiorMenorTest.java
+++ src/main/java/br/com/caelum/aula/MaiorMenorTest.java
@@ -2,40 +2,50 @@ package br.com.caelum.aula;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MaiorMenorTest {
 	
-	@Test
-	public void numerosEmQualquerOrdem() {
-		MaiorMenor alg = new MaiorMenor();
-		alg.encontra(new int[]{4, 15, 7, 8});
-		Assert.assertEquals(15, alg.getMaior());
-		Assert.assertEquals(4, alg.getMenor());
+	private MaiorMenor mm;
+	private static int cont;
+	
+	@Before
+	public void setUp(){
+		this.mm = new MaiorMenor();
+		cont += 1;
 	}
 	
 	@Test
-	public void numerosOrdemCrescente() {
-		MaiorMenor alg = new MaiorMenor();
-		alg.encontra(new int[]{1, 5, 8, 10, 15});
-		Assert.assertEquals(15, alg.getMaior());
-		Assert.assertEquals(1, alg.getMenor());
+	public void numerosEmQualquerOrdem() {	
+		mm.encontra(new int[]{4, 15, 7, 8});
+		Assert.assertEquals(15, mm.getMaior());
+		Assert.assertEquals(4, mm.getMenor());
+		System.out.println(cont);
 	}
 	
 	@Test
-	public void numerosEmOrdemDecrescente() {
-		MaiorMenor alg = new MaiorMenor();
-		alg.encontra(new int[]{15, 7, 8, 2, 1});
-		Assert.assertEquals(15, alg.getMaior());
-		Assert.assertEquals(1, alg.getMenor());
+	public void numerosOrdemCrescente() {		
+		mm.encontra(new int[]{1, 5, 8, 10, 15});
+		Assert.assertEquals(15, mm.getMaior());
+		Assert.assertEquals(1, mm.getMenor());
+		System.out.println(cont);
 	}
 	
 	@Test
-	public void testeUnicoNumero() {
-		MaiorMenor alg = new MaiorMenor();
-		alg.encontra(new int[]{1});
-		Assert.assertEquals(1, alg.getMaior());
-		Assert.assertEquals(1, alg.getMenor());
+	public void numerosEmOrdemDecrescente() {	
+		mm.encontra(new int[]{15, 7, 8, 2, 1});
+		Assert.assertEquals(15, mm.getMaior());
+		Assert.assertEquals(1, mm.getMenor());
+		System.out.println(cont);
+	}
+	
+	@Test
+	public void testeUnicoNumero() {	
+		mm.encontra(new int[]{1});
+		Assert.assertEquals(1, mm.getMaior());
+		Assert.assertEquals(1, mm.getMenor());
+		System.out.println(cont);
 	}
 	
 }
